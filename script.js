7c4c2a6b5a8b1108d0df19772f4deb99e882d06d
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-11-24 15:07:53 -0600

    Can make an API call, and return an obj

--- script.js
+++ script.js
@@ -2,11 +2,58 @@
 
 
 const myKey = "cb15120a874ba7b3d3c9ff343bd7f119"
-const urlPrim = (`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${myKey}`)
-const urlSnd =  (`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${myKey}`)
+// const urlPrim = (`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${myKey}`)
+// const urlSnd =  (`https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${myKey}`)
 // const apiRoot = (`https://api.openweathermap.org`)
 
 
+
+
+const searchInput = document.querySelector('#locationInputtext')
+const searchbtn= document.querySelector('#searchButton')
+
+
+//make a call to the api with the search button info
+//template taken from inclass 10===================
+function getcurrentweather(searchCity) {
+   
+    
+    const requestUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${myKey}`)
+    
+
+    fetch(requestUrl)
+        .then(function (response) {
+            console.log(requestUrl, "URL in use")
+            return response.json();
+        })
+        // .then(function (data) {
+        //     //Using console.log to examine the data
+        //     console.log(data);
+        //     for (var i = 0; i < data.length; i++) {
+        //         //Creating a h3 element and a p element
+        //         var userName = document.createElement('h3');
+        //         var userUrl = document.createElement('p');
+
+        //         //Setting the text of the h3 element and p element.
+        //         userName.textContent = data[i].login;
+        //         userUrl.textContent = data[i].url;
+
+        //         //Appending the dynamically generated html to the div associated with the id="users"
+        //         //Append will attach the element as the bottom most child.
+        //         usersContainer.append(userName);
+        //         usersContainer.append(userUrl);
+        //     }
+        // });
+}
+
+
+function handleSearchFormSubmit(e) {
+    e.preventDefault();
+    var search = searchInput.value.trim();
+    getcurrentweather(search) 
+};
+
+searchbtn.addEventListener('click', handleSearchFormSubmit);
 
 
 // GIVEN a weather dashboard with form inputs
