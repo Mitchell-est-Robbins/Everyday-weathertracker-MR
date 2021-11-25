388214e7b9e20652a2623f9484f08a52e5b06042
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-11-24 23:43:15 -0600

    api call data 1/2 partial render

--- script.js
+++ script.js
@@ -2,33 +2,76 @@
 
 
 const myKey = "cb15120a874ba7b3d3c9ff343bd7f119"
-// const urlPrim = (`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${myKey}`)
-// const urlSnd =  (`https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${myKey}`)
+// const urlPrim = (`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=imperial&appid=${myKey}`)
+// const urlSnd =  (`https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&units=imperial&appid=${myKey}`)
 // const apiRoot = (`https://api.openweathermap.org`)
 
-
-
+// global variables for use in the template
+let cName;
+let date;
+let temp;
+let humid;
+let wind;
+let wpic;
+let cityw;
 
 const searchInput = document.querySelector('#locationInputtext')
 const searchbtn= document.querySelector('#searchButton')
 
 
+
+
+
+
+
 //make a call to the api with the search button info
 //template taken from inclass 10===================
+// current weather api call===============================================================================
 function getcurrentweather(searchCity) {
    
     
-    const requestUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${myKey}`)
-    
+    const requestUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=imperial&appid=${myKey}`)
+    document.querySelector('#cityName').textContent = '';
+    // document.querySelector('#theDate').textContent = '';
+    document.querySelector('#cityTemp').textContent = '';
+    document.querySelector('#cityHumid').textContent = '';
+    document.querySelector('#cityWind').textContent = '';
+    document.querySelector('#cityweather').textContent = '';
+    // document.querySelector('#wIcon').textContent = '';
 
+    
     fetch(requestUrl)
         .then(function (response) {
             console.log(requestUrl, "URL in use")
+            // console.log(response)
             return response.json();
         })
+        .then(function (response) {
+// ------------values from data
+            console.log(response)
+            cName=response.name;
+            // date=
+            temp=response.main.temp
+            humid=response.main.humidity
+            wind=response.wind.speed
+            cityw=response.weather[0].description
+            // wpic=
+// -----------change values
+        document.querySelector('#cityName').textContent = cName;
+         // document.querySelector('#theDate').textContent = '';
+        document.querySelector('#cityTemp').textContent = "Temperature: " +temp+ " F ";
+        document.querySelector('#cityHumid').textContent = "Humidity: " +humid+ " % ";
+        document.querySelector('#cityWind').textContent = "Windspeed: " +wind+ " m/ph ";
+        document.querySelector('#cityweather').textContent = cityw;
+        // document.querySelector('#wIcon').textContent = '';
+
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
         // .then(function (data) {
-        //     //Using console.log to examine the data
-        //     console.log(data);
+        //     //     console.log(data);
+            // -----function for rendering the data
         //     for (var i = 0; i < data.length; i++) {
         //         //Creating a h3 element and a p element
         //         var userName = document.createElement('h3');
@@ -46,17 +89,47 @@ function getcurrentweather(searchCity) {
         // });
 }
 
+// =======================5 day forcast api call=========================
+function getWeatherForcast(searchCity) {
 
+
+    const requestUrl = (`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&units=imperial&appid=${myKey}`)
+
+
+    fetch(requestUrl)
+        .then(function (response) {
+            console.log(requestUrl, "URL2 in use")
+            // console.log(response)
+            return response.json();
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+
+}
+
+
+// render response into something that can be used.
+// ==================== render data================================
+
+// ===================search btn handler========================================
 function handleSearchFormSubmit(e) {
     e.preventDefault();
     var search = searchInput.value.trim();
     getcurrentweather(search) 
+    getWeatherForcast (search)
 };
 
 searchbtn.addEventListener('click', handleSearchFormSubmit);
 
 
-// GIVEN a weather dashboard with form inputs
+// <h2 id='cityName'>x</h2>
+// <p id='theDate'>x</p>
+// <p id='cityTemp'>x</p>
+// <p id='cityHumid'>x</p>
+// <p id='cityWind'></p>
+
+
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and 
